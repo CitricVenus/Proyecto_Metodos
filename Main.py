6d7fd5f662315805eb883a3a0796c7e84598e206
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2022-06-01 23:33:28 -0500

    Se agrego mas contenido para datos

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from ast import If
 from datetime import date
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -6,8 +7,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+prediction_G = 0
+prediction_PG = 0
+prediction_PG13 = 0
+prediction_R = 0
+prediction_TV14 = 0
+prediction_TVG = 0
+prediction_TVMA = 0
+prediction_TVPG = 0
+prediction_TVY = 0
+prediction_TVY7= 0
+prediction_NC17 = 0
+suma_ninos = 0
+suma_adolescentes = 0
+suma_adultos = 0
+suma_total = 0
 
-anio = 2030
+anio = 2022
 df = pd.read_csv('netflix_titles.csv', usecols=('show_id', 'date_added', 'rating'))
 df['date_added'] = pd.DatetimeIndex(pd.to_datetime(df['date_added'])).year
 #year = pd.DatetimeIndex(pd.to_datetime(df['date_added'])).year
@@ -26,6 +42,7 @@ tv_ma = df.loc[df["rating"]=="TV-MA"]
 tv_pg = df.loc[df["rating"]=="TV-PG"]
 tv_y = df.loc[df["rating"]=="TV-Y"]
 tv_y7 = df.loc[df["rating"]=="TV-Y7"]
+nc_17 = df.loc[df["rating"]=="NC-17"]
 
 
 g_anio = g.groupby(["date_added"],as_index=False).count().drop("show_id",axis=1)
@@ -38,6 +55,7 @@ tvma_anio = tv_ma.groupby(["date_added"],as_index=False).count().drop("show_id",
 tvpg_anio = tv_pg.groupby(["date_added"],as_index=False).count().drop("show_id",axis=1)
 tvy_anio = tv_y.groupby(["date_added"],as_index=False).count().drop("show_id",axis=1)
 tvy7_anio = tv_y7.groupby(["date_added"],as_index=False).count().drop("show_id",axis=1)
+nc17_anio = nc_17.groupby(["date_added"],as_index=False).count().drop("show_id",axis=1)
 
 
 
@@ -92,7 +110,7 @@ def prediccionRating(df,title,anio):
     plt.title(title)
     
     plt.show()
-
+    return prediccion.round(2)
  
 
 
@@ -122,13 +140,51 @@ def prediccionRating(df,title,anio):
     #crear dataframe con dos columnas para cada rating con año y cantidad de peliculas.
     
 
-prediccionRating(g_anio,"Rating G",anio)
-prediccionRating(pg_anio,"Rating PG",anio)
-prediccionRating(pg13_anio,"Rating PG-13",anio)
-prediccionRating(r_anio,"Rating R",anio)
-prediccionRating(tv14_anio,"Rating TV-14",anio)
-prediccionRating(tvg_anio,"Rating TV-G",anio)
-prediccionRating(tvma_anio,"Rating TV-MA",anio)
-prediccionRating(tvpg_anio,"Rating TV-PG",anio)
-prediccionRating(tvy_anio,"Rating TV-Y",anio)
-prediccionRating(tvy7_anio,"Rating TV-Y7",anio)
+prediction_G = prediccionRating(g_anio,"Rating G",anio)
+prediction_PG = prediccionRating(pg_anio,"Rating PG",anio)
+prediction_PG13 = prediccionRating(pg13_anio,"Rating PG-13",anio)
+prediction_R = prediccionRating(r_anio,"Rating R",anio)
+prediction_TV14 = prediccionRating(tv14_anio,"Rating TV-14",anio)
+prediction_TVG = prediccionRating(tvg_anio,"Rating TV-G",anio)
+prediction_TVMA = prediccionRating(tvma_anio,"Rating TV-MA",anio)
+prediction_TVPG = prediccionRating(tvpg_anio,"Rating TV-PG",anio)
+prediction_TVY = prediccionRating(tvy_anio,"Rating TV-Y",anio)
+prediction_TVY7 = prediccionRating(tvy7_anio,"Rating TV-Y7",anio)
+prediction_NC17 = prediccionRating(nc17_anio,"Rating NC-17",anio)
+
+suma_ninos = prediction_TVY + prediction_TVY7 + prediction_G + prediction_TVG + prediction_PG + prediction_TVPG
+
+suma_adolescentes = prediction_PG13 + prediction_TV14
+
+suma_adultos = prediction_R + prediction_TVMA + prediction_NC17
+
+suma_total = suma_ninos + suma_adolescentes + suma_adultos
+
+print("-----------------------------Resultados finales------------------------------")
+print("Niños = " + str(suma_ninos))
+print("Adolescentes = " + str(suma_adolescentes))
+print("Adultos = " + str(suma_adultos))
+
+def mayorContenido():
+    if suma_ninos>suma_adolescentes and suma_ninos>suma_adultos:
+        print("En el año " + str(anio) + " la audiencia con mayor contenido en Netflix es Niños" )
+    if suma_adolescentes>suma_ninos and suma_adolescentes>suma_adultos:
+        print("En el año " + str(anio) + " la audiencia con mayor contenido en Netflix es Adolescentes" )
+    if suma_adultos>suma_ninos and suma_adultos>suma_adolescentes:
+        print("En el año " + str(anio) + " la audiencia con mayor contenido en Netflix es Adultos" )
+
+def menorContenido():
+    if suma_ninos<suma_adolescentes and suma_ninos<suma_adultos:
+        print("En el año " + str(anio) + " la audiencia con menor contenido en Netflix es Niños" )
+    if suma_adolescentes<suma_ninos and suma_adolescentes<suma_adultos:
+        print("En el año " + str(anio) + " la audiencia con menor contenido en Netflix es Adolescentes" )
+    if suma_adultos<suma_ninos and suma_adultos<suma_adolescentes:
+        print("En el año " + str(anio) + " la audiencia con menor contenido en Netflix es Adultos" )
+
+
+
+mayorContenido()
+menorContenido()
+print("-------------------------------Total de contenido de Netflix en "+str(anio)+"---------------------------------------")
+print(str(suma_total))
+
